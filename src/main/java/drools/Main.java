e3ff8d231a7a61de2f0d629fdc80aaff19c417c2
kubzdel <kubzdel@gmail.com>
2017-11-22 00:00:43 +0100

    Initial commit

---
+++ src/main/java/drools/Main.java
@@ -0,0 +1,28 @@
+package drools;
+
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.drools.compiler.compiler.DroolsParserException;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+public class Main {
+    public static void main(String[] args) throws DroolsParserException,
+            IOException {
+        Main main = new Main();
+        main.executeHelloWorldRules();
+    }
+    public void executeHelloWorldRules() throws IOException,
+            DroolsParserException {
+        KieServices ks = KieServices.Factory.get();
+        BasicConfigurator.configure();
+        Logger.getLogger(Main.class).setLevel(Level.OFF);
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession session = kContainer.newKieSession("ksession-rules");
+      // tutaj dodać obiekty session.insert
+        session.fireAllRules();
+    }
+}
